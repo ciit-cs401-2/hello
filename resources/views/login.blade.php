c7c33f5a6d298d49a70cbfeb9877636012fedcf3
clyde123545 <clyde.ador@ciit.edu.ph>
2025-07-31 09:43:38 +0800

    try commit

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,20 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Login</h1>
-    <form action="/login" method="POST">
-        @csrf
-        <div>
-            <label for="username">Username</label>
-            <input type="text" name="username">
-        </div>
 
-        <div>
-            <label for="password">Password</label>
-            <input type="password" name="password">
-        </div>
+    <div class="min-h-screen flex items-center justify-center">
 
-        <button type="submit">Login</button>
-    </form>
-    <a href="/register">Don't Have an Account?</a>
+        <h1>Login</h1>
+        <form action="/login" method="POST" class="p-5 bg-red-500">
+            @csrf
+            <div>
+                <label for="username">Username</label>
+                <input type="text" name="username">
+            </div>
+    
+            <div>
+                <label for="password">Password</label>
+                <input type="password" name="password">
+            </div>
+    
+            <button type="submit">Login</button>
+        </form>
+        <a href="/register">Don't Have an Account?</a>
+    </div>
 @endsection
