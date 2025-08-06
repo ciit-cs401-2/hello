259f45d856a2a2ec67f8dbdc9a3697efd7ac3130
clyde123545 <clyde.ador@ciit.edu.ph>
2025-08-06 16:00:40 +0800

    Login and Register UI

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,24 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
+<link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <div class="login-container ">
 
-    <div class="min-h-screen flex items-center justify-center">
-
-        <h1>Login</h1>
-        <form action="/login" method="POST" class="p-5 bg-red-500">
+        
+        <form action="/login" method="POST" class="">
             @csrf
-            <div>
+            <div class="logo">
+                <div class="logo-icon"></div>
+                <h1>FinOut</h1>
+                <p class="tagline">Your Daily Edge in Finance</p>
+            </div>
+            <div class="form-group full-width">
                 <label for="username">Username</label>
                 <input type="text" name="username">
             </div>
     
-            <div>
+            <div class="form-group full-width">
                 <label for="password">Password</label>
                 <input type="password" name="password">
             </div>
     
-            <button type="submit">Login</button>
+            <button type="submit" class="submit-btn">Login</button>
+            <div class="login-link">
+                <a href="/register">Don't Have an Account?</a>
+            </div>
         </form>
-        <a href="/register">Don't Have an Account?</a>
+        
     </div>
 @endsection
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,35 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Registration</h1>
-    <form action="/register" method="POST">
-        @csrf
-        <div>
-            <label for="firstName">First Name</label>
-            <input type="text" name="firstName" />
-        </div>
+<link rel="stylesheet" href="{{ asset('css/registration.css') }}">
+    <div class="registration-container w-screen h-screen">
+        
+        <form action="/register" method="POST" class="">
+            @csrf
+            <div class="logo">
+                <div class="logo-icon"></div>
+                <h1>FinOut</h1>
+                <p class="tagline">Your Daily Edge in Finance</p>
+            </div>
+            <div class="form-row">
+                <div class="form-group">
+                    <label for="firstName">First Name</label>
+                    <input type="text" name="firstName" id="firstName" placeholder="Katya" />
+                </div>
+                <div class="form-group">
+                    <label for="lastName">Last Name</label>
+                    <input type="text" name="lastName" id="lastName" placeholder="Romana" />
+                </div>
+            </div>
 
-        <div>
-            <label for="lastName">Last Name</label>
-            <input type="text" name="lastName">
-        </div>
+            <div class="form-group full-width">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" placeholder="Katbus" />
+            </div>
 
-        <div>
-            <label for="email">Email</label>
-            <input type="email" name="email">
-        </div>
+            <div class="form-group full-width">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" placeholder="katbus8008@gmail.com" />
+            </div>
 
-        <div>
-            <label for="username">Username</label>
-            <input type="text" name="username" />
-        </div>
+            <div class="form-group full-width">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" placeholder="••••••••••••••" />
+            </div>
 
-        <div>
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password">
-        </div>
+            <button type="submit" class="submit-btn">Register</button>
+            
+            <div class="login-link">
+                <a href="/login">Already have an account? Log In</a>
+            </div>
+        </form>
 
-        <button type="submit">SUBMIT</button>
-    </form>
-    <a href="/login">Already Have an Account?</a>
+    </div>
 @endsection
